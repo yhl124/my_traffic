f9b335ed46bd1afb9e0e832d6404547b34a49e3f
yh <yhl124.py@gmail.com>
2024-03-26 01:02:24 +0900

    인코딩해결은 된것같다 https://codekodo.tistory.com/185

--- my_traffic/widgtes/BustopSearch.swift
+++ my_traffic/widgtes/BustopSearch.swift
@@ -30,8 +30,8 @@ struct BusStopData: Codable {
                 
                 struct Item: Codable, Identifiable {
                     var id = UUID()
-                    let gpslati: Double
-                    let gpslong: Double
+                    //let gpslati: Double
+                    //let gpslong: Double
                     let nodeid: String
                     let nodenm: String
                     let nodeno: Int
@@ -44,70 +44,82 @@ struct BusStopData: Codable {
 struct BusStopSearch {
     func fetchBusStopData(query: String, completion: @escaping (Result<BusStopData, Error>) -> Void) {
         let apiKey = KeyOutput.getAPIKey(for: "BusStopSearch")
-        print(apiKey)
+        //print(apiKey)
+        let pageNo = "1"
+        let numOfRows = "10"
+        let _type = "json"
+        let cityCode = "31160"
+        let nodeNm = "금정"
         //let decodedApiKey = apiKey.removingPercentEncoding ?? apiKey
-        let baseURL = "https://apis.data.go.kr/1613000/BusSttnInfoInqireService/getSttnNoList?"
+        let baseURL = "https://apis.data.go.kr/1613000/BusSttnInfoInqireService/getSttnNoList?serviceKey=\(apiKey)&pageNo=\(pageNo)&numOfRows=\(numOfRows)&_type=\(_type)&cityCode=\(cityCode)&nodeNm=\(nodeNm)"
         
-        let queryItems = [
-            //URLQueryItem(name: "serviceKey", value: decodedApiKey),
-            URLQueryItem(name: "serviceKey", value: apiKey),
-            URLQueryItem(name: "pageNo", value: "1"),
-            URLQueryItem(name: "numOfRows", value: "10"),
-            URLQueryItem(name: "_type", value: "json"),
-            URLQueryItem(name: "cityCode", value: "31160"),
-            URLQueryItem(name: "nodeNm", value: "금정"),
-            //URLQueryItem(name: "nodeNm", value: query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
-        ]
+        guard let encodedStr = baseURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
+        let encodedAndReplacingStr = encodedStr.replacingOccurrences(of: "%25", with: "%")
         
-        print(queryItems)
+        guard let url = URL(string: encodedAndReplacingStr) else {return}
         
-        var components = URLComponents(string: baseURL)
-        components?.queryItems = queryItems
+//        let queryItems = [
+//            //URLQueryItem(name: "serviceKey", value: decodedApiKey),
+//            URLQueryItem(name: "serviceKey", value: apiKey),
+//            URLQueryItem(name: "pageNo", value: "1"),
+//            URLQueryItem(name: "numOfRows", value: "10"),
+//            URLQueryItem(name: "_type", value: "json"),
+//            URLQueryItem(name: "cityCode", value: "31160"),
+//            URLQueryItem(name: "nodeNm", value: "금정"),
+//            //URLQueryItem(name: "nodeNm", value: query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
+//        ]
         
-        guard let url = components?.url else {
-            completion(.failure(NSError(domain: "BusStopSearch", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
-            return
-        }
-
+//        print(queryItems)
+        
+//        var components = URLComponents(string: baseURL)
+//        components?.queryItems = queryItems
+//        
+//        print(queryItems)
+        
+//        guard let url = components?.url else {
+//            completion(.failure(NSError(domain: "BusStopSearch", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+//            return
+//        }
+     
+        
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
         print(request)
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data, let responseString = String(data: data, encoding: .utf8) {
-                    print("Response Data: \(responseString)")
-                } else if let error = error {
-                    print("Error: \(error.localizedDescription)")
-                }
-            }
-        
 //        URLSession.shared.dataTask(with: request) { data, response, error in
-//            if let error = error {
-//                completion(.failure(error))
-//                return
+//            DispatchQueue.main.async {
+//                if let data = data, let responseString = String(data: data, encoding: .utf8) {
+//                    print("Response Data: \(responseString)")
+//                } else if let error = error {
+//                    print("Error: \(error.localizedDescription)")
+//                }
 //            }
-//            
-//            guard let data = data else {
-//                let error = NSError(domain: "BusStopSearch", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])
-//                completion(.failure(error))
-//                return
-//            }
-//            
-//            // Print JSON data
-//            if let jsonString = String(data: data, encoding: .utf8) {
-//                print("JSON Response: \(jsonString)")
-//            }
-//            
-//            do {
-//                let response = try JSONDecoder().decode(BusStopData.self, from: data)
-//                // 여기서 response.response.body.items.item에 접근하여 원하는 값들을 가져올 수 있습니다.
-//                print("Response Data: \(response)")
-//                completion(.success(response))
-//            } catch {
-//                completion(.failure(error))
-//            }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "BusStopSearch", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])
+                completion(.failure(error))
+                return
+            }
+            
+            // Print JSON data
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print("JSON Response: \(jsonString)")
+            }
+            
+            do {
+                let response = try JSONDecoder().decode(BusStopData.self, from: data)
+                // 여기서 response.response.body.items.item에 접근하여 원하는 값들을 가져올 수 있습니다.
+                completion(.success(response))
+            } catch {
+                completion(.failure(error))
+            }
         }.resume()
     }
 }
