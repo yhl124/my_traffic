f080d72841351a3056c9fe278f3fcf06f1d2fd45
yh <yhl124.py@gmail.com>
2024-04-14 03:46:10 +0900

    안되는 이유를 모르겠다. 그리고 coredata에 노선들이 안지워진다

--- my_traffic/widgtes/CustomSheetStyle.swift
+++ my_traffic/widgtes/CustomSheetStyle.swift
@@ -1,10 +1,10 @@
+////
+////  CustomSheetStyle.swift
+////  my_traffic
+////
+////  Created by yhl on 2024/04/01.
+////
 //
-//  CustomSheetStyle.swift
-//  my_traffic
-//
-//  Created by yhl on 2024/04/01.
-//
-
 import Foundation
 import SwiftUI
 
@@ -45,33 +45,33 @@ struct CustomSheetStyle<Content: View>: View {
 }
 
 
-//struct CustomSheetStyle<Content: View>: View {
-//    let content: Content
-//    let title: String
-//    let onDismiss: () -> Void
-//    let onConfirm: () -> Void
-//    let selectedStationName: String // 선택한 정류장 이름
-//    let selectedMobileNo: String // 선택한 정류장 번호
-//    
-//
-//    var body: some View {
-//        NavigationView {
-//            VStack {
-//                Text("\(selectedStationName) (\(selectedMobileNo))")
-//                    .font(.title3)
-//
-//                content
-//                    .navigationBarItems(
-//                        leading: Button(action: onDismiss) {
-//                            Text("닫기")
-//                        },
-//                        trailing: Button(action: onConfirm) {
-//                            Text("확인")
-//                        }
-//                    )
-//            }
-//            .navigationBarTitle(title)
-//            .navigationBarTitleDisplayMode(.inline)
-//        }
-//    }
-//}
+////struct CustomSheetStyle<Content: View>: View {
+////    let content: Content
+////    let title: String
+////    let onDismiss: () -> Void
+////    let onConfirm: () -> Void
+////    let selectedStationName: String // 선택한 정류장 이름
+////    let selectedMobileNo: String // 선택한 정류장 번호
+////    
+////
+////    var body: some View {
+////        NavigationView {
+////            VStack {
+////                Text("\(selectedStationName) (\(selectedMobileNo))")
+////                    .font(.title3)
+////
+////                content
+////                    .navigationBarItems(
+////                        leading: Button(action: onDismiss) {
+////                            Text("닫기")
+////                        },
+////                        trailing: Button(action: onConfirm) {
+////                            Text("확인")
+////                        }
+////                    )
+////            }
+////            .navigationBarTitle(title)
+////            .navigationBarTitleDisplayMode(.inline)
+////        }
+////    }
+////}
