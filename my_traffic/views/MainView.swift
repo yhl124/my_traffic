e73f420b613956f676d3b4995994485169d56386
yh <yhl124.py@gmail.com>
2024-05-11 23:35:35 +0900

    일단 mainview를 쪼갬

--- my_traffic/views/MainView.swift
+++ my_traffic/views/MainView.swift
@@ -11,130 +11,110 @@ import CoreData
 
 struct MainView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    @StateObject private var busRealTimeViewModel = BusRealTimeViewModel() // BusRealTimeViewModel 추가
+    @StateObject private var busRealTimeViewModel = BusRealTimeViewModel()
     
-    @FetchRequest(
-        sortDescriptors: [],
-        animation: .default)
+    @FetchRequest(sortDescriptors: [], animation: .default)
     private var busStops: FetchedResults<BusStop>
     
     var body: some View {
         NavigationStack {
             ZStack {
-                Color(UIColor.systemGray6).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                BackgroundView()
                 VStack {
-                    List {
-                        ForEach(busStops) { busStop in
-                            VStack(alignment: .leading) {
-                                Text("\(busStop.stationName ?? "Unknown") (\(busStop.mobileNo ?? "No Mobile No"))")
-                                    .font(.headline)
-                                
-                                // XML 데이터를 가져오는 부분
-                                ForEach(Array(busStop.routes as? Set<BusRoute> ?? []), id: \.self) { route in
-                                    VStack(alignment: .leading) {
-                                        HStack {
-                                            Text("\(route.routeName ?? "") (\(route.routeTypeCd ?? ""))")
-                                            Spacer()
-                                            // 실시간 정보를 표시합니다.
-                                            if let realTimeInfo = busRealTimeViewModel.busRealTimeInfos.first(where: { $0.routeId == route.routeId }) {
-                                                HStack {
-                                                    Text("\(realTimeInfo.predictTime1)분 ")
-                                                    Text("\(realTimeInfo.predictTime2)분")
-                                                }
-                                            } else {
-                                                Text("실시간 정보 없음")
-                                                    .foregroundColor(.red)
-                                            }
-                                        }
-                                    }
-                                }
-                                .onAppear {
-                                    busRealTimeViewModel.fetchRealTimeInfo()
-                                }
-                            }
-                        }
-            
-//                    List {
-//                        ForEach(busStops) { busStop in
-//                            VStack(alignment: .leading) {
-//                                Text("\(busStop.stationName ?? "Unknown") (\(busStop.mobileNo ?? "No Mobile No"))")
-//                                    .font(.headline)
-//                                ForEach(Array(busStop.routes as? Set<BusRoute> ?? []), id: \.self) { route in
-//                                    VStack(alignment: .leading) {
-//                                        HStack {
-//                                            Text("\(route.routeName ?? "") (\(route.routeTypeCd ?? ""))")
-//                                            Spacer()
-//                                            if let realTimeInfo = busRealTimeViewModel.busRealTimeInfos.first(where: { $0.routeId == route.routeId }) {
-//                                                HStack {
-//                                                    Text("\(realTimeInfo.predictTime1)분 ")
-//                                                    Text("\(realTimeInfo.predictTime2)분")
-//                                                }
-//                                            } else {
-//                                                Text("실시간 정보 없음")
-//                                                    .foregroundColor(.red) // 원하는 스타일 및 색상으로 설정
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    List {
-//                        ForEach(busStops) { busStop in
-//                            VStack(alignment: .leading) {
-//                                Text("\(busStop.stationName ?? "Unknown") (\(busStop.mobileNo ?? "No Mobile No"))")
-//                                    .font(.headline)
-//                                ForEach(Array(busStop.routes as? Set<BusRoute> ?? []), id: \.self) { route in
-//                                    Text("\(route.routeName ?? "") - \(route.routeTypeCd ?? "")")
-//                                }
-//                            }
-//                        }
-                        .onDelete { indexSet in
-                            for index in indexSet {
-                                let busStop = busStops[index]
-                                viewContext.delete(busStop)
-                            }
-                            do {
-                                try viewContext.save()
-                            } catch {
-                                print("Error deleting bus stop: \(error.localizedDescription)")
-                            }
-                        }
-                    }
-                    .listStyle(PlainListStyle())
-                    .listRowInsets(EdgeInsets()) // 리스트 행의 좌우 여백 제거
-                    .padding(.top)
-                    
-                    HStack(alignment: .bottom) {
-                        NavigationLink(destination: SubwayView()) {
-                            ZStack {
-                                Circle()
-                                    .foregroundColor(.blue)
-                                    .frame(width: 60, height: 60)
-                                Image(systemName: "train.side.front.car")
-                                    .foregroundColor(.white)
-                            }
-                        }
-                        Spacer()
-                        NavigationLink(destination: BusView()) {
-                            ZStack {
-                                Circle()
-                                    .foregroundColor(.blue)
-                                    .frame(width: 60, height: 60)
-                                Image(systemName: "bus")
-                                    .foregroundColor(.white)
-                            }
-                        }
-                    }
-                    .padding()
+                    BusStopListView(busStops: busStops, viewContext: viewContext)
+                    NavigationButtons()
                 }
             }
             .navigationTitle("위젯 설정")
-            .navigationBarItems(
-                trailing: NavigationLink(destination: SettingView()) {
-                    Image(systemName: "gear")
-                }
-            )
-//            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing: SettingButton())
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        Color(UIColor.systemGray6).edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct BusStopListView: View {
+    var busStops: FetchedResults<BusStop>
+    var viewContext: NSManagedObjectContext
+    
+    var body: some View {
+        List {
+            ForEach(busStops) { busStop in
+                BusStopView(busStop: busStop)
+            }
+            .onDelete { indexSet in
+                deleteBusStops(at: indexSet)
+            }
+        }
+        .listStyle(PlainListStyle())
+        .listRowInsets(EdgeInsets())
+        .padding(.top)
+    }
+    
+    private func deleteBusStops(at indexSet: IndexSet) {
+        for index in indexSet {
+            let busStop = busStops[index]
+            viewContext.delete(busStop)
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            print("Error deleting bus stop: \(error.localizedDescription)")
+        }
+    }
+}
+
+struct BusStopView: View {
+    let busStop: BusStop
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(busStop.stationName ?? "Unknown") (\(busStop.mobileNo ?? "No Mobile No"))")
+                .font(.headline)
+            ForEach(Array(busStop.routes as? Set<BusRoute> ?? []), id: \.self) { route in
+                Text("\(route.routeName ?? "") - \(route.routeTypeCd ?? "")")
+            }
+        }
+    }
+}
+
+struct NavigationButtons: View {
+    var body: some View {
+        HStack(alignment: .bottom) {
+            NavigationLink(destination: SubwayView()) {
+                NavigationButton(icon: "train.side.front.car")
+            }
+            Spacer()
+            NavigationLink(destination: BusView()) {
+                NavigationButton(icon: "bus")
+            }
+        }
+        .padding()
+    }
+}
+
+struct NavigationButton: View {
+    var icon: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(.blue)
+                .frame(width: 60, height: 60)
+            Image(systemName: icon)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct SettingButton: View {
+    var body: some View {
+        NavigationLink(destination: SettingView()) {
+            Image(systemName: "gear")
         }
     }
 }
